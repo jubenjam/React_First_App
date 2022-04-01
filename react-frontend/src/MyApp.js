abe755d31c515fd2ebf35aab10f2cb27908cea8f
jubenjam <49247806+jubenjam@users.noreply.github.com>
2022-04-01 12:55:37 -0700

    Delete Button

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
 const characters = [
     {
@@ -21,11 +21,36 @@ const characters = [
 ];
 
 function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+            name: 'Charlie',
+            job: 'Janitor',
+        },
+        {
+            name: 'Mac',
+            job: 'Bouncer',
+        },
+        {
+            name: 'Dee',
+            job: 'Aspring actress',
+        },
+        {
+            name: 'Dennis',
+            job: 'Bartender',
+        },
+    ]);
+    function removeOneCharacter (index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index
+        });
+        setCharacters(updated);
+    }
     return (
         <div className="container">
-            <Table characterData={characters} />
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
         </div>
-    );  
+    )  
 }
+  
 
 export default MyApp;
--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -6,6 +6,7 @@ function TableHeader()  {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>Remove</th>
             </tr>
         </thead>
     );
@@ -15,8 +16,11 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
-                <td>{row.name}</td>         
+                <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         );
     }
@@ -29,11 +33,11 @@ function TableBody(props) {
 }
   
   
-function Table(props) {
+function Table (props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData={props.characterData} />
+            <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
         </table>
     );
 }
