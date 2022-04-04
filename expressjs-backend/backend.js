5c265b52959495a2e2af2120a29518eacfd71161
jubenjam <49247806+jubenjam@users.noreply.github.com>
2022-04-04 13:03:06 -0700

    started backend.js

---
+++ expressjs-backend/backend.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});      
+
+const users = { 
+    users_list :
+    [
+        { 
+            id : 'xyz789',
+            name : 'Charlie',
+            job: 'Janitor',
+        },
+        {
+            id : 'abc123', 
+            name: 'Mac',
+            job: 'Bouncer',
+        },
+        {
+            id : 'ppp222', 
+            name: 'Mac',
+            job: 'Professor',
+        }, 
+        {
+            id: 'yat999', 
+            name: 'Dee',
+            job: 'Aspring actress',
+        },
+        {
+            id: 'zap555', 
+            name: 'Dennis',
+            job: 'Bartender',
+        }
+    ]
+}
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
