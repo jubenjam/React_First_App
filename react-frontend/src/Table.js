76076f0187addd1dd5df7c9617fc938ab89d0acb
jubenjam <49247806+jubenjam@users.noreply.github.com>
2022-04-06 14:41:11 -0700

    ID column

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -4,6 +4,7 @@ function TableHeader()  {
     return (
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Name</th>
                 <th>Job</th>
                 <th>Remove</th>
@@ -16,6 +17,7 @@ function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
+                <td>{row.id}</td>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
                 <td>
